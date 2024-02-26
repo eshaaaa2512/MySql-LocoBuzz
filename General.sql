2072df927e9ffb5289d28bad1b0206e51c2a5b33
Esha <eshadeswal@gmail.com>
2024-02-26 10:15:24 +0530

    [ESHA] ADD: Basic concept like declaring,set,incre*decr

--- General.sql
+++ General.sql
@@ -1,22 +1,13 @@
----1)WHAT IS DATABASE?A database is information that is set up for easy access, management and updating. Computer databases typically store aggregations of data records or files that contain information, such as sales transactions, customer data, financials and product information.
+USE PRACTICE
 
----2)WHAT ARE DATABASE USED FOR? improve bussiness processes,keep track of customers,secure personal health information,store personal data.
+DECLARE @NAME VARCHAR(50), @AGE INT;
+SET @NAME = 'ESHA';
+SELECT @NAME;
 
----3)TYPES OF DATABASE? 
---retional database (This tabular approach defines data so it can be reorganized and accessed in many ways. )
+DECLARE @NAME VARCHAR(50), @AGE INT;
+SELECT @NAME = 'DESWAL',@AGE =25;
+SELECT @NAME AS 'NAME',@AGE AS 'AGE';
 
---distributed database (This database stores records or files in several physical locations. Data processing is also spread out and replicated across different parts of the network.)
-
---cloud ( These databases are built in a public, private or hybrid cloud for a virtualized environment. Users are charged based on how much storage and bandwidth they use. )
-
---NoSQL( NoSQL databases are good when dealing with large collections of distributed data.)
-
---Object-oriented (These databases hold data created using object-oriented programming languages. They focus on organizing objects rather than actions and data rather than logic. For instance, an image data record would be a data object, rather than an alphanumeric value.)
-
---Graph (These databases are a type of NoSQL database. They store, map and query relationships using concepts from graph theory.)
-
----4)WHAT ARE DATABASE CHALLENGES? data security, data integrity,database performance, database integration
-
----5) difference between mysql and sql?Mysql is a software and sql is a database language.
-
----6) what is difference between relational and non relational database ? table format and Documents
+DECLARE @NUMBER INT  = 100;
+SET @NUMBER += 50;
+SELECT @NUMBER;
