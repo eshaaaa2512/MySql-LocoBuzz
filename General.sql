e0db09187bd69fccd51997513919805dd6224430
Esha <eshadeswal@gmail.com>
2024-03-08 12:11:00 +0530

    [ESHA] ADD: snapshots

--- General.sql
+++ General.sql
@@ -1,16 +1,13 @@
 USE INFO_DB;
-CREATE TABLE sample
-( Product Varchar(10),
-    Region Varchar (5),
-    Revenue int)
-INSERT INTO sample VALUES('A','EAST',100),('B','WEST',200),('C','NORTH',150),('D','SOUTH',120)
 
-SELECT * FROM SAMPLE;
 
-SELECT 
-    Product,
-    [EAST], [WEST], [NORTH], [SOUTH]
-FROM 
-    (SELECT Product, Region, Revenue FROM sample) AS SourceTable
-PIVOT
-    (SUM(Revenue) FOR Region IN ([EAST], [WEST], [NORTH], [SOUTH])) AS PivotTable;
+---database snapshot
+
+CREATE DATABASE EMP_SNAPS ON
+(NAME =INFO_DB,
+FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQL16MSSQLSERVER02\MSSQL\DATA\info_db.ss')
+AS SNAPSHOT OF INFO_DB;
+GO
+
+
+ 
